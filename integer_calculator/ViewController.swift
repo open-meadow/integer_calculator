c667e8f181180b74b40847835baf700bbb3ec687
open-meadow <rohan.varughese200@gmail.com>
2025-09-23 19:11:55 +0530

    Get calculator working successfully

--- integer_calculator/ViewController.swift
+++ integer_calculator/ViewController.swift
@@ -11,9 +11,44 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        displayLabel.text = "0"
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet var displayLabel: UILabel!
+    
+    var operations: [String] = []
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        let buttonContent: String = (sender.titleLabel?.text)!
+        
+        if buttonContent != "=" {
+            operations.append(buttonContent)
+            displayLabel.text = operations.joined(separator: " ")
+        } else {
+            let opValue = performOperation(operations)
+            operations = []
+            displayLabel.text = opValue
+        }
+    }
+    
+    func performOperation(_ operations: [String]) -> String {
+        let num1: Int = Int(operations[0]) ?? 0
+        let op = operations[1]
+        let num2: Int = Int(operations[2]) ?? 0
+        
+        switch op {
+        case "+":
+            return String(num1 + num2)
+        case "-":
+            return String(num1 + num2)
+        case "*":
+            return String(num1 * num2)
+        case "/":
+            return String(num1 / num2)
+        default:
+            return "Too complex"
+        }
+    }
 }
 
