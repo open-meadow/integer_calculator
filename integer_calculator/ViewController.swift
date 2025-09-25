662412c3b4eaccde0488037c0b1aa0a4a2dd8e71
open-meadow <rohan.varughese200@gmail.com>
2025-09-25 22:54:14 +0530

    Add comments

--- integer_calculator/ViewController.swift
+++ integer_calculator/ViewController.swift
@@ -26,19 +26,26 @@ class ViewController: UIViewController {
         
         if buttonContent != "=" {
             if(buttonContent == "C") {
+                // If user presses 'C', empty all display
                 prevScreenText = ""
                 screenText = ""
                 displayLabel.text = "0"
             } else if (buttonContent == "+" || buttonContent == "-" || buttonContent == "/" || buttonContent == "*") {
+                // If the user presses either of the operations, save last number on the screen
                 operation = buttonContent
                 prevScreenText = screenText
                 displayLabel.text = buttonContent
                 screenText = ""
             } else {
+                // If the user presses a number, show the number on the screen
+                if screenText == "Zero Division" || screenText == "Cannot parse input" || screenText == "0" {
+                    screenText = ""
+                }
                 screenText += buttonContent
                 displayLabel.text = screenText
             }
         } else {
+            // If user presses '=', previous number, operation, and current number
             let opValue = performOperation(prevScreenText, operation, screenText)
             prevScreenText = screenText
             screenText = opValue
@@ -47,6 +54,7 @@ class ViewController: UIViewController {
     }
     
     func performOperation(_ prevScreenText: String, _ operation: String, _ screenText: String) -> String {
+        // If any number is not provided, select '0'
         let num1: Int = Int(prevScreenText) ?? 0
         let op = operation
         let num2: Int = Int(screenText) ?? 0
@@ -60,7 +68,7 @@ class ViewController: UIViewController {
             return String(num1 * num2)
         case "/":
             if (num2 == 0) {
-                return "Zero division"
+                return "Zero Division"
             } else {
                 return String(num1 / num2)
             }
