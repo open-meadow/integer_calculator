11dcde7441fdefd51bf80c4791d83547884b45e4
open-meadow <rohan.varughese200@gmail.com>
2025-09-25 22:34:15 +0530

    Implement calculation of multi-digit numbers

--- integer_calculator/ViewController.swift
+++ integer_calculator/ViewController.swift
@@ -17,42 +17,55 @@ class ViewController: UIViewController {
     
     @IBOutlet var displayLabel: UILabel!
     
-    var operations: [String] = []
+    var screenText: String = ""
+    var prevScreenText: String = ""
+    var operation: String = ""
     
     @IBAction func calculate(_ sender: UIButton) {
         let buttonContent: String = (sender.titleLabel?.text)!
         
         if buttonContent != "=" {
             if(buttonContent == "C") {
-                operations = []
+                prevScreenText = ""
+                screenText = ""
                 displayLabel.text = "0"
+            } else if (buttonContent == "+" || buttonContent == "-" || buttonContent == "/" || buttonContent == "*") {
+                operation = buttonContent
+                prevScreenText = screenText
+                displayLabel.text = buttonContent
+                screenText = ""
             } else {
-                operations.append(buttonContent)
-                displayLabel.text = operations.joined(separator: " ")
+                screenText += buttonContent
+                displayLabel.text = screenText
             }
         } else {
-            let opValue = performOperation(operations)
-            operations = []
-            displayLabel.text = opValue
+            let opValue = performOperation(prevScreenText, operation, screenText)
+            prevScreenText = screenText
+            screenText = opValue
+            displayLabel.text = screenText
         }
     }
     
-    func performOperation(_ operations: [String]) -> String {
-        let num1: Int = Int(operations[0]) ?? 0
-        let op = operations[1]
-        let num2: Int = Int(operations[2]) ?? 0
+    func performOperation(_ prevScreenText: String, _ operation: String, _ screenText: String) -> String {
+        let num1: Int = Int(prevScreenText) ?? 0
+        let op = operation
+        let num2: Int = Int(screenText) ?? 0
         
         switch op {
         case "+":
             return String(num1 + num2)
         case "-":
-            return String(num1 + num2)
+            return String(num1 - num2)
         case "*":
             return String(num1 * num2)
         case "/":
-            return String(num1 / num2)
+            if (num2 == 0) {
+                return "Zero division"
+            } else {
+                return String(num1 / num2)
+            }
         default:
-            return "Too complex"
+            return "Cannot parse input"
         }
     }
 }
